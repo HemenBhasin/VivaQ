60cf8104b41c8e202bd6d424bb4b9c400d4e7338
HEMEN BHASIN <hemenbhasin@gmail.com>
2025-08-06 13:11:23 +0530

    first commit

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./config/vivaq-416-firebase-adminsdk-fbsvc-36a9787496.json');
+const serviceAccount = require('./config/vivaq-416-firebase-adminsdk-fbsvc-3e2b310d3e.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
